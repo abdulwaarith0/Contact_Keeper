b3478741bc4d73fe836bc67596ee9bc6f39c2209
Abdulwaarith~z <abdulwaarithz@gmail.com>
2023-04-03 11:37:07 +0100

    completed Navbar and router setup

--- routes/contacts.js
+++ routes/contacts.js
@@ -23,8 +23,36 @@ router.get("/", auth, async (req, res) => {
 // @route   POST /api/contacts
 // @desc    Add contact
 // @access  Private 
-router.post("/", (req, res) => {
-    res.send("Add contact");
+router.post("/", [auth, [
+    check("name", "Name is required").notEmpty()
+]], async (req, res) => {
+    // Check for errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+    const { name, email, phone, type } = req.body;
+
+    // Create new contact
+    try {
+        const newContact = new Contact({
+            name,
+            email,
+            phone,
+            type,
+            user: req.user.id
+        });
+
+        // Save contact to database
+        const contact = await newContact.save();
+        res.json(contact);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
 });
 
 // @route   PUT /api/contacts/:id

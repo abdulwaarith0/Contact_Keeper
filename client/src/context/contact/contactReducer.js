3d3f2ca1005cc5b8237ca4b14b577219909f71e4
Abdulwaarith~z <abdulwaarithz@gmail.com>
2023-04-03 22:18:10 +0100

    Added contact filter form and state

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -20,11 +20,11 @@ const reducer = (state, action) => {
         case UPDATE_CONTACT:
             return {
                 ...state,
-                contacts: 
+                contacts:
                     state.contacts.map((contact) => contact.id ===
                         action.payload.id ? action.payload : contact
                     )
-            }; 
+            };
         case DELETE_CONTACT:
             return {
                 ...state,
@@ -42,6 +42,20 @@ const reducer = (state, action) => {
             return {
                 ...state,
                 current: null
+            };
+        case FILTER_CONTACTS:
+            return {
+                ...state,
+                filtered: state.contacts.filter(contact => {
+                    const regex = new RegExp(`${action.payload}`, "gi");
+                    return contact.name.match(regex) ||
+                        contact.email.match(regex);
+                })
+            };
+        case CLEAR_FILTER:
+            return {
+                ...state, 
+                filtered: null
             };
         default:
             return state;
